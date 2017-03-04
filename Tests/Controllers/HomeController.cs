4d2ccfafdb0df24ed8da738071910a1218fbce89
yogendra tamang <yogendra tamang>
2017-03-05 00:39:01 +0545

    ASPNET MVC Autocomplete with XML Data

---
+++ Tests/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using Tests.Models;
+
+namespace Tests.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(string Prefix)
+        {
+            IEnumerable<IncomeInfo> incomeTypes = XDocument.Load(Server.MapPath("~/Resources/IncomeOptions.xml")).Element("IncomeTypes")
+                       .Descendants("IncomeType")
+                       .Select(x => new IncomeInfo
+                       {
+                           IncomeTitle = x.Element("Name").Value,
+                           IncomeType = x.Element("Type").Value
+
+                       });
+            //ViewBag.LoanTypes = loanTypes;
+           
+            //Note : you can bind same list from database  
+            
+
+            
+            return Json(incomeTypes, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Tests/Models/IncomeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tests.Models
+{
+    public class IncomeInfo
+    {
+        public string IncomeTitle { get; set; }
+        public string IncomeType { get; set; }
+    }
+}
